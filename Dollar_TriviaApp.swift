5464c041e5b1f7649c651e30893310b51410544b
visveee <77760877+visveee@users.noreply.github.com>
2024-11-11 21:56:42 -0600

    Add files via upload

---
+++ Dollar_TriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dollar_TriviaApp.swift
+//  Dollar Trivia
+//
+//  Created by Vishal Veeramachaneni on 10/21/24.
+//
+
+import SwiftUI
+
+@main
+struct Dollar_TriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
